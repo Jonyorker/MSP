5d6c033915343d2aacc03f85efe1c46a53376766
Jon York <jonyorker@gmaill.com>
2020-09-22 23:51:39 -0400

    remove sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,14 +4,13 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Laravel\Sanctum\HasApiTokens;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, HasApiTokens, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
